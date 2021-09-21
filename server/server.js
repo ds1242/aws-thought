7a5872b25342864f57ffb2eca31ab4462e62e1fb
ds1242 <david.shaw1242@gmail.com>
2021-09-20 18:15:52 -0600

    added get route for all thoughts

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+  app.use(express.static('client/build'));
 }
 
 // app.use(require('./routes'));
